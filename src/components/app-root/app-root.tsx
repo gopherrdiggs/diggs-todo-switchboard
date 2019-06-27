258c17289ac9ee4ca0f29de5c914058c855d291d
grodgers <grodgers@GR20170626>
2019-06-27 12:30:39 -0500

    Remove commented code

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,22 +1,6 @@
 import { Component, h, Element, Listen } from '@stencil/core';
-// import { IAppState, ITodoItem } from "../../interfaces/app-interfaces";
 import { SwitchboardOperator } from '../../state/switchboard-operator';
 import { AppStateController, AppState, Actions } from '../../state/app-state';
-
-// interface ActionCallbackMapping {
-//   eventName: string,
-//   callback: Function
-// }
-
-// interface ElementActionCallbackMapping {
-//   elementId: string,
-//   callbacks: ActionCallbackMapping[]
-// }
-
-// enum Actions {
-//   todoItemChecked = 'todoItemChecked',
-//   todoItemUnchecked = 'todoItemUnchecked'
-// }
 
 @Component({
   tag: 'app-root',
@@ -26,131 +10,16 @@ export class AppRoot {
 
   @Element() el: HTMLAppRootElement;
 
-  // private appState = {} as IAppState;
-  // private actionCallbackRegistry = [] as ElementActionCallbackMapping[];
-
   async componentWillLoad() {
 
     await AppStateController.initializeController();
     SwitchboardOperator.setRootElement(this.el);
-    // await this.getInitialState();
   }
 
-  // async getInitialState() {
-
-  //   // TODO: Retrieve initial state from persisted storage
-  //   this.appState = {
-  //     incompleteTodos: {
-  //       items: [
-  //         { id: '09dsht', summary: 'Test 2', isComplete: false },
-  //         { id: 'lfa09h', summary: 'Test 3', isComplete: false },
-  //         { id: 'flk2jg', summary: 'Test 4', isComplete: false }
-  //       ],
-  //       count: 0
-  //     },
-  //     completeTodos: {
-  //       items: [
-  //         { id: 'l1kj3t', summary: 'Test 1', isComplete: true }
-  //       ],
-  //       count: 0
-  //     }
-  //   }
-  // }
-
-  // Action handler functions
-
-  // handleTodoItemCheckedChanged(event: any) {
-
-  //   let item = event.detail.item as ITodoItem;
-
-  //   // console.log('Handling item checked: ', item);
-
-  //   if (item.isComplete) {
-
-  //     // Remove item from incomplete items list
-  //     this.appState.incompleteTodos.items =
-  //       this.appState.incompleteTodos.items.filter(i => {
-  //         return i.id != item.id;
-  //       });
-  //     this.appState.incompleteTodos.count =
-  //       this.appState.incompleteTodos.items.length;
-  //     // Add item to complete items list
-  //     this.appState.completeTodos.items =
-  //       [...this.appState.completeTodos.items, item];
-  //     this.appState.completeTodos.count =
-  //       this.appState.completeTodos.items.length;
-
-  //     // Notify subscribers to event todoItemChecked
-  //     this.executeElementEventCallbacks(Actions.todoItemChecked);
-  //   }
-  //   else {
-
-  //     // Remove item from complete items list
-  //     this.appState.completeTodos.items =
-  //       this.appState.completeTodos.items.filter(i => {
-  //         return i.id != item.id;
-  //       });
-  //     this.appState.completeTodos.count =
-  //       this.appState.completeTodos.items.length;
-  //     // Add item to incomplete items list
-  //     this.appState.incompleteTodos.items =
-  //       [...this.appState.incompleteTodos.items, item];
-  //     this.appState.incompleteTodos.count =
-  //       this.appState.incompleteTodos.items.length;
-
-  //     // Notify subscribers to event todoItemUnchecked
-  //     this.executeElementEventCallbacks(Actions.todoItemUnchecked);
-  //   }
-
-  // }
-
-  // async registerElementEventCallback(elementId: string,
-  //   eventName: string, callback: Function) {
-
-  //   let elemEventCallbacks = this.actionCallbackRegistry.find(e => {
-  //     return e.elementId === elementId
-  //   });
-
-  //   if (!elemEventCallbacks) {
-
-  //     this.actionCallbackRegistry.push({
-  //       elementId: elementId,
-  //       callbacks: []
-  //     });
-
-  //     elemEventCallbacks = this.actionCallbackRegistry.find(e => {
-  //       return e.elementId === elementId
-  //     });
-  //   }
-
-  //   let eventCallbackRegistration = elemEventCallbacks.callbacks.find(c => {
-  //     return c.eventName === eventName;
-  //   });
-
-  //   if (!eventCallbackRegistration) {
-
-  //     elemEventCallbacks.callbacks.push({
-  //       eventName: eventName,
-  //       callback: callback
-  //     });
-  //   }
-
-  // }
-
-  // async executeElementEventCallbacks(eventName: string) {
-
-  //   console.log('Executing callbacks for event: ', eventName);
-
-  //   for (let r of this.actionCallbackRegistry) {
-  //     for (let cb of r.callbacks) {
-  //       if (cb.eventName === eventName) {
-  //         console.log(`Calling ${cb.callback.toString()} on ${r.elementId}`);
-  //         await cb.callback();
-  //       }
-  //     }
-  //   }
-  // }
-
+  // As elements are added to the DOM, this method picks out certain ones, identified by the element's ID property,
+  // then registers Events=>Actions and Actions=>Methods mappings for the element (web component)
+  // Obviously, the registration of all these elements and callbacks doesn't scale well for a single file like this,
+  // so any ideas on how to organize this logic - maybe based on domain - would be appreciated.
   @Listen('DOMNodeInserted', { target: 'document' })
   handleDomNodeInserted(event: any) {
 
@@ -166,7 +35,7 @@ export class AppRoot {
 
       case 'incompleteItemsList': {
 
-        let list = element as HTMLTodoListElement;
+        const list = element as HTMLTodoListElement;
         // Events => Actions
         SwitchboardOperator.registerElementEventToStateActionHandler(
           'onItemCheckedChanged', 
@@ -189,7 +58,7 @@ export class AppRoot {
 
       case 'completedItemsList': {
 
-        let list = element as HTMLTodoListElement;
+        const list = element as HTMLTodoListElement;
         // Events => Actions
         SwitchboardOperator.registerElementEventToStateActionHandler(
           'onItemCheckedChanged', 
@@ -208,7 +77,7 @@ export class AppRoot {
 
       case 'incompleteItemsCount': {
 
-        let badge = element as HTMLToolbarBadgeElement;
+        const badge = element as HTMLToolbarBadgeElement;
         // Events => Actions
         // ...none
         // Actions => Methods
@@ -227,7 +96,7 @@ export class AppRoot {
 
       case 'completedItemsCount': {
 
-        let badge = element as HTMLToolbarBadgeElement;
+        const badge = element as HTMLToolbarBadgeElement;
         // Events => Actions
         // ...none
         // Actions => Methods
@@ -244,82 +113,12 @@ export class AppRoot {
         break;
       }
     }
-
-    // if (element.id === 'incompleteItemsList') {
-
-    //   let elem = element as HTMLTodoListElement;
-
-    //   // Map from component events to state mangement action handlers
-    //   this.el.addEventListener('onItemCheckedChanged', (e: any) => {
-    //     // Take item from event detail, pass to state mgmt handler
-    //     this.handleTodoItemCheckedChanged(e);
-    //   });
-
-    //   // Map from state management actions to component methods
-    //   this.registerElementEventCallback(
-    //     element.id,
-    //     Actions.todoItemChecked,
-    //     () => {
-    //       elem.setTodos(this.appState.incompleteTodos.items);
-    //     });
-
-    //   elem.setTodos(this.appState.incompleteTodos.items);
-
-    // }
-    // else if (element.id === 'completedItemsList') {
-
-    //   let elem = element as HTMLTodoListElement;
-
-    //   // Map from component events to state mangement action handlers
-    //   this.el.addEventListener('onItemCheckedChanged', (e: any) => {
-    //     // Take item from event detail, pass to state mgmt handler
-    //     this.handleTodoItemCheckedChanged(e);
-    //   });
-
-    //   // Map from state management actions to component methods
-    //   this.registerElementEventCallback(
-    //     element.id,
-    //     Actions.todoItemChecked,
-    //     () => {
-    //       elem.setTodos(this.appState.completeTodos.items);
-    //     });
-
-    //   elem.setTodos(this.appState.completeTodos.items);
-    // }
-    // else if (element.id === 'incompleteItemsCount') {
-
-    //   let elem = element as HTMLToolbarBadgeElement;
-
-    //   // Map from state management actions to component methods
-    //   this.registerElementEventCallback(
-    //     element.id,
-    //     Actions.todoItemChecked,
-    //     () => {
-    //       elem.setContent(this.appState.incompleteTodos.count);
-    //     });
-
-    //   elem.setContent(this.appState.incompleteTodos.items.length);
-    // }
-    // else if (element.id === 'completedItemsCount') {
-      
-    //   let elem = element as HTMLToolbarBadgeElement;
-
-    //   // Map from state management actions to component methods
-    //   this.registerElementEventCallback(
-    //     element.id,
-    //     Actions.todoItemUnchecked,
-    //     () => {
-    //       elem.setContent(this.appState.completeTodos.count);
-    //     });
-
-    //   elem.setContent(this.appState.completeTodos.items.length);
-    // }
   }
 
   @Listen('DOMNodeRemoved', { target: 'document' })
   handleDomNodeRemoved(_event: any) {
 
-    //TODO: Unregister callbacks ?
+    //TODO: Unregister element events and methods from state management
   }
 
   render() {
